a9b61c50797dc44c8938b8dc45eacef09de9b99b
Decovista <sales.decovista@gmail.com>
2026-01-28 11:49:01 +0530

    setting  up Athra Project

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-black flex justify-center items-center min-h-[100vh]">
+      <h1 className="text-white text-3xl">Hii Athra</h1>
+    </div>
+  );
+}
