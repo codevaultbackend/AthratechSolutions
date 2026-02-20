7cc33ad2564cbe92a7de2e839fc81cfa4093a77b
Decovista <sales.decovista@gmail.com>
2026-02-20 10:59:45 +0530

    added facebook pexel meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,21 +9,11 @@ export const metadata: Metadata = {
   title: "Athratech Tech",
   description: "Provides various tech solutions",
 
-
   icons: {
     icon: [
-      {
-        url: "/favicon.ico",
-      },
-      {
-        url: "/favicon-96x96.png",
-        sizes: "96x96",
-        type: "image/png",
-      },
-      {
-        url: "/favicon.svg",
-        type: "image/svg+xml",
-      },
+      { url: "/favicon.ico" },
+      { url: "/favicon-96x96.png", sizes: "96x96", type: "image/png" },
+      { url: "/favicon.svg", type: "image/svg+xml" },
     ],
     apple: "/apple-touch-icon.png",
   },
@@ -41,24 +31,35 @@ export default function RootLayout({
       lang="en"
       className={`${inter.variable} ${bricolage.variable} ${calligraffitti.variable}`}
     >
-      <body className="antialiased bg-white max-w-[1440px] w-full mx-auto">
-        {/* Navbar */}
-        <TopNavigation />
-
-        {/* Page content */}
-        <main className="relative">{children}</main>
-
-        {/* Footer */}
-        <Footer />
-
+      <head>
         {/* Google Analytics */}
-        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-1PLCHP3548"></Script>
-        <Script>
-          {` window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-1PLCHP3548"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-1PLCHP3548');
+          `}
+        </Script>
 
-  gtag('config', 'G-1PLCHP3548');`}
+        {/* Meta Pixel Code */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '25984887041146356');
+            fbq('track', 'PageView');
+          `}
         </Script>
 
         {/* Font Awesome */}
@@ -69,7 +70,28 @@ export default function RootLayout({
           crossOrigin="anonymous"
           referrerPolicy="no-referrer"
         />
+      </head>
 
+      <body className="antialiased bg-white max-w-[1440px] w-full mx-auto">
+        {/* Navbar */}
+        <TopNavigation />
+
+        {/* Page content */}
+        <main className="relative">{children}</main>
+
+        {/* Footer */}
+        <Footer />
+
+        {/* Meta Pixel Noscript Fallback */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=25984887041146356&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
       </body>
     </html>
   );
